3adbbe69f263ef12d8903bbb7db51eae98797269
gerardkra30@gmail.com <gerardkra30@gmail.com>
2026-01-13 11:19:14 +0000

    Saved web app Geo Analysis (NETWORKOPTIMIZATION.Kp1SNbR)

--- web_apps/Kp1SNbR/app.js
+++ web_apps/Kp1SNbR/app.js
@@ -1,28 +1,26 @@
-dataiku.fetch('accidents_database_prepared', {
-    sampling : "random",
-    limit : 10000
-  }, function (df) {
+dataiku.fetch('rental_agencies_geocode', {
+    sampling : "head",
+    limit : 20000
+}, function (df) {
 
-    // Add a map marker for each row on the dataset
-    // Each marker is a circle. The size of the circle varies with the 'size' column values
     var nbRows = df.getNbRows();
     for (var i = 0; i < nbRows; i++) {
-      var record = df.getRecord(i);
+        var record = df.getRecord(i);
 
-      // Replace by your own column names here
-      var lat = parseFloat(record["latitude"]);
-      var lon = parseFloat(record["longitude"]);
-      var collision = record["collision"];
+        var lat = parseFloat(record["geolatitude"]);
+        var lon = parseFloat(record["geolongitude"]);
+        var name = record["agency_name"];
+        var city = record["city"];
+        if (isNaN(lat) || isNaN(lon)) continue;
 
-      // Radius of the marker is in meters
-      var radius = 1;
+        var radius = 15000;
 
-      var marker = new L.circle([lat, lon], radius, {
-          color: 'red',
-          fillColor: 'red',
-          fillOpacity: 0.2
-        }).bindPopup("Number of collisions: <strong>" + collision + "</strong>");
+        var marker = new L.circle([lat, lon], radius, {
+            color: 'green',
+            fillColor: 'green',
+            fillOpacity: 0.2
+        }).bindPopup("Name: <strong>" + name + "</strong>");
 
         marker.addTo(map);
-      };
+    }
 });
